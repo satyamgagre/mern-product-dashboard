08de7c773ab2205235a6964f6e380d1fc44d509d
Satyam Gagare <satyamgagre4@gmail.com>
2025-12-28 00:35:48 +0530

    28-12-2025

---
+++ backend/models/product.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required : true
+    },
+    price: {
+        type : Number,
+        required : true
+    }, 
+    description: {
+        type : String,
+        required : true
+    },
+    imageUrl: {
+        type : String,
+        required : true
+    }
+}, { timestamps: true });  //createdA and updatedAt
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,54 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Server is ready.");
+app.use(express.json()); // allow to accept JSON data in request body
+
+  app.get('/api/products', async (req, res) => {
+    try {
+      const products = await Product.find({});
+      res.status(200).json({success: true, data : products});
+    } catch (error) {
+      console.log("Error in Get Products:", error.message);
+      res.status(500).json({success: false, message: "Failed to fetch products."});
+    }
+  });
+
+app.post('/api/products', async (req, res) => {
+  const product = req.body; // user will send product data in request body
+
+  if (!product.name|| !product.price || !product.description || !product.imageUrl) {
+    return res.status(400).json({success: false, message: "All product fields are required."});
+  }
+
+  const newProduct = new Product(product)
+
+  try {
+    await newProduct.save();
+    res.status(201).json({success: true, data : newProduct});
+  } catch (error) {
+    console.log("Error in Create Product:", error.message);
+    res.status(500).json({success: false, message: "Failed to create product."});  
+  }
+
 });
+
+  app.delete('/api/products/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+      await Product.findByIdAndDelete(id);
+      res.status(200).json({success: true, message: "Product deleted successfully."});
+    } catch (error) {
+      console.log("Error in Delete Product:", error.message);
+      res.status(500).json({success: false, message: "Product not found."});
+    }
+  });
+
 const PORT = process.env.PORT || 5000;
 // Connect DB → then start server
 connectDB()
